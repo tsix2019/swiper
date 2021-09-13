72beee82229ed18b6c6e5e0b246dcf27b8890059
Tsix666 <tyj2019@qq.com>
2021-09-13 11:43:24 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <swiper>
+      <SwiperItem v-for="(item, index) in goods" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </SwiperItem>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "./components/swiper/index";
+export default {
+  data() {
+    return {
+      goods: [
+        { link: "https://www.mi.com", image: "https://s3.bmp.ovh/imgs/2021/09/1cb4db872ddb0e17.jpg" },
+        { link: "https://www.mi.com", image: "https://s3.bmp.ovh/imgs/2021/09/1cb4db872ddb0e17.jpg" },
+        { link: "https://www.mi.com", image: "https://s3.bmp.ovh/imgs/2021/09/1cb4db872ddb0e17.jpg" },
+        { link: "https://www.mi.com", image: "https://s3.bmp.ovh/imgs/2021/09/1cb4db872ddb0e17.jpg" },
+      ],
+    };
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
+
+<style scoped>
+  SwiperItem{
+    height: 300px;
+  }
+</style>
